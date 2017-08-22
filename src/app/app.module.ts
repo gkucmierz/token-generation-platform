f56c813db6e2cf67d62a3e872d8eae30ab983c37
gkucmierz <gkucmierz@gmail.com>
2017-08-23 01:42:03 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import 'hammerjs';
+import { CreateTokenComponent } from './create-token/create-token.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'create-token', component: CreateTokenComponent },
+  { path: '',
+    component: HomeComponent
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTokenComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
